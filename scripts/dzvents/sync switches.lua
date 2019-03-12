da0ab590b4eddaaebe6b67ded47fb3a06587585f
rrozema <1747982+rrozema@users.noreply.github.com>
2019-03-12 04:00:09 +0100

    Update sync switches.lua

--- scripts/dzvents/sync switches.lua
+++ scripts/dzvents/sync switches.lua
@@ -1,31 +1,26 @@
-local D1 = 'Keuken: Aanrecht'
-local D2 = 'Keuken: S3'
-
 return {
 	on = {
 		devices = {
-			*
+			'*'
 		}
 	},
     execute = function(domoticz, device, triggerInfo)
 		if (domoticz.EVENT_TYPE_DEVICE == triggerInfo.type) then
-			if device.Type = 'Light/Switch' 
-					and device.SubType = 'Switch' 
-					and device.description ~= nil 
-					and device.description ~= '' 
+			if device.deviceType == "Light/Switch"
+					and device.description ~= nil
+					and device.description ~= ""
 			then
 				local description = device.description
 				local state = device.state
 				local ok, settings = pcall( domoticz.utils.fromJSON, device.description)
     	        if ok and settings ~= nil then
 					local sync_group = settings.sync_group
-					if sync_group ~= nil and sync_group ~= '' then
+					if sync_group ~= nil and sync_group ~= "" then
 						domoticz.devices().forEach(
 							function(otherdevice)
-								if otherdevice.Type = 'Light/Switch' 
-									and otherdevice.SubType = 'Switch' 
-									and otherdevice.description ~= nil 
-									and otherdevice.state ~= state 
+								if otherdevice.deviceType == "Light/Switch"
+									and otherdevice.description ~= nil
+									and otherdevice.state ~= state
 								then
 									local ok, othersettings = pcall( domoticz.utils.fromJSON, otherdevice.description)
 									if ok and othersettings ~= nil then
@@ -37,8 +32,8 @@ return {
 							end
 						)
 					end
-				end
-			end
+			    end
+		    end
         end
-	end
+    end
 }
